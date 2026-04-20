8dd12cff53c2ad3b8ae0196d81265197e44d532b
yoel agirretxe vilar <agirretxe.147247@e.unavarra.es>
2026-04-20 20:06:56 +0200

    Stack Navigation

--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,5 +1,7 @@
+import { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Card, Text } from 'react-native-paper';
+import { EXCURSIONES } from '../comun/excursiones';
 
 function RenderExcursion(props) {
   const excursion = props.excursion;
@@ -7,7 +9,11 @@ function RenderExcursion(props) {
   if (excursion != null) {
     return (
       <Card style={styles.card}>
-        <Card.Title title={excursion.nombre} />
+        <Card.Title
+          title={excursion.nombre}
+          titleStyle={styles.titulo}
+          style={styles.cardTitle}
+        />
         <Card.Cover
           source={require('./imagenes/40Años.png')}
           style={styles.image}
@@ -24,8 +30,19 @@ function RenderExcursion(props) {
   }
 }
 
-function DetalleExcursion(props) {
-  return <RenderExcursion excursion={props.excursion} />;
+class DetalleExcursion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      excursiones: EXCURSIONES
+    };
+  }
+
+  render() {
+    const { excursionId } = this.props.route.params;
+
+    return <RenderExcursion excursion={this.state.excursiones[+excursionId]} />;
+  }
 }
 
 const styles = StyleSheet.create({
@@ -38,6 +55,12 @@ const styles = StyleSheet.create({
   descripcion: {
     marginTop: 20,
     marginBottom: 20,
+  },
+  titulo: {
+    textAlign: 'center',
+  },
+  cardTitle: {
+    alignItems: 'center',
   },
 });
 
